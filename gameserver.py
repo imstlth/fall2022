7c93ecf155b293d391573ebb8b4e1173e356e805
imstlth <58philou@free.fr>
2022-12-17 13:54:43 +0100

    Ajout de commentaires.
    NOTE : À ce moment, le code sur CG n'est pas identique à celui de waiting.py

--- gameserver.py
+++ gameserver.py
@@ -19,6 +19,10 @@ bots_pos = {
 # La quantité de matiériaux pour chaque joueur
 territory, recyclers, matter = (eval(repr(bots_pos)),) * 3  # Le "template" est le même que bots_pos
 
+# Cette variable est l'emplacement des bots au tout départ
+# Elle est utile pour plusieurs fonctions.
+star = ((-1, 0), (0, -1), (1, 0), (0, 1))  # (0, 0) n'est pas dedans car la case ne contient pas de bots
+
 # Return une liste plus son symétrique (utile pour la génération de la map)
 def symetry(liste):
     return liste + liste[::-1]
@@ -41,33 +45,54 @@ def gen_starting():
     while True:
         starting_robots = [random.randint(1, height - 2), random.randint(1, height - 1)]  # Il y a minimum 2 d'écart avec la ligne du milieu
         # On vérifie chaque case de l'"étoile"
-        star = ((-1, 0), (0, -1), (1, 0), (0, 1))  # (0, 0) n'est pas dedans car la case ne contient pas de bots
         for x, y in star + ((0, 0),):
             # Si une des cases est de l'herbe, on relance une tentative
             if grid_scrap[starting_robots[1] + y][starting_robots[0] + x] == 0:
                 break
         else:
+            # On rajoute chacune des positions de l'étoile (autour du point central)
             bots_pos["self"] = [[starting_robots[0] + x, starting_robots[1] + y] for x, y in star]
+            # Les bots ennemis sont sur le même y, mais à l'opposé au niveau de x
             bots_pos["ennemi"] = [[width - bot_x, bot_y] for bot_x, bot_y in bots_pos["self"]]
+            # Les emplacements des bots sont forcément dans le territoire
             territory = eval(repr(bots_pos))
+            # On rajoute la case centrale (starting_robots)
             territory["self"].append([starting_robots[0], starting_robots[1]])
             territory["ennemi"].append([width - starting_robots[0], starting_robots[1]])
             break
 
+# La fonction qui remplace "input"
+# On compte le nombre de fois que cette fonction a été appellée
+# afin de renvoyer les informations qui conviennent à chaque fois.
 input_count = -2
 def game_input():
     global input_count
 
+    # Le premier input demande toujours le width et le height de la map
     if input_count == -2:
         input_count = -1
         return str(width) + " " + str(height)
 
     else:
+        # On augment le compteur mais on le remet à 0 lorsque toutes les cases de la map ont été faites
         input_count = (input_count + 1) % ((width + 1) * (height + 1))
 
+        # On calcule à partir du compteur, la ligne et la colonne de la case.
         line = input_count // (width + 1)
         col = input_count % (width + 1)
+
+        # On récupère les informations.
+
+        # Si la case est recyclée par un recycler.
+        for x, y in star + ((0, 0),):  # De nouveau, on utilise star
+            if [col + x, line + y] in recyclers["self"] or [col + x, line + y] in recyclers["ennemi"]:
+                is_recycled = "1"
+                break
+        else:
+            is_recycled = "0"
+
         scrap_amount = grid_scrap[line][col]
+        owner = "-1"
         if [col, line] in territory["self"]:
             owner = "1"
             owner_code = "self"
@@ -75,21 +100,12 @@ def game_input():
             owner = "0"
             owner_code = "ennemi"
         else:
-            owner = "-1"
-
-        for x, y in ((-1, 0), (0, -1), (1, 0), (0, 1)):
-            if [col + x, line + y] in recyclers["self"] or [col + x, line + y] in recyclers["ennemi"]:
-                is_recycled = "1"
-                break
-        else:
-            is_recycled = "0"
-
-        if owner != "-1":
-            units = bots_pos[owner_code].count([col, line])
-            recycler = "1" if [col, line] in recyclers[owner_code] else "0"
-            can_build = "1" if scrap_amount != 0 and units == 0 and [col, line] in territory["self"] else "0"
-            can_spawn = "1" if scrap_amount != 0 and recycler != "1" and [col, line] in territory["self"] else "0"
-
-            return f"{str(scrap_amount)} {owner} {str(units)} {recycler} {can_build} {can_spawn} {is_recycled}"
-        else:
             return str(scrap_amount) + " -1 0 0 0 0 " + is_recycled
+
+        # Les informations pour chaque case
+        units = bots_pos[owner_code].count([col, line])
+        recycler = "1" if [col, line] in recyclers[owner_code] else "0"
+        can_build = "1" if scrap_amount != 0 and units == 0 and [col, line] in territory["self"] else "0"
+        can_spawn = "1" if scrap_amount != 0 and recycler != "1" and [col, line] in territory["self"] else "0"
+
+        return f"{str(scrap_amount)} {owner} {str(units)} {recycler} {can_build} {can_spawn} {is_recycled}"
